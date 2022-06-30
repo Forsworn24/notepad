b4696afc79cb12b0e172f3ff23356e05caebc6a7
Forsworn24 <78202543+Forsworn24@users.noreply.github.com>
2022-06-30 20:35:37 +0500

    notepad version 1.0

--- post.rb
+++ post.rb
@@ -1,5 +1,13 @@
 class Post
 
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+    return post_types[type_index].new
+  end
+
   def initialize
     @created_at = Time.now
     @text = nil
@@ -14,13 +22,12 @@ class Post
   end
 
   def save
-    file = File.new(file_path, "w:UTF-8")
+    file = File.new(file_path, 'w:UTF-8') # открываем файл на запись
 
-    for item in to_strings do
-      file puts(item)
-    end
+    to_strings.each { |string| file.puts(string) }
 
     file.close
+
   end
 
   def file_path
